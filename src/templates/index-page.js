0cfaf9f4e5166a051d4ddb8f38ba4c630d636b21
Harvey Ramer <harvey@harveyramer.com>
2020-01-03 22:20:53 -0600

    Color changes.

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -42,8 +42,8 @@ export const IndexPageTemplate = ({
             className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
             style={{
               boxShadow:
-                'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-              backgroundColor: 'rgb(202, 100, 99)',
+                'rgb(0, 159, 254) 0.5rem 0px 0px, rgb(0, 159, 254) -0.5rem 0px 0px',
+              backgroundColor: 'rgb(0, 159, 254)',
               color: 'white',
               lineHeight: '1',
               padding: '0.25em',
@@ -55,8 +55,8 @@ export const IndexPageTemplate = ({
             className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
             style={{
               boxShadow:
-                'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-              backgroundColor: 'rgb(202, 100, 99)',
+                'rgb(0, 159, 254) 0.5rem 0px 0px, rgb(0, 159, 254) -0.5rem 0px 0px',
+              backgroundColor: 'rgb(0, 159, 254)',
               color: 'white',
               lineHeight: '1',
               padding: '0.25em',
