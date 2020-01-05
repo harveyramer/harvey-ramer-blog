7acb4493921caec7a5746fd23560b6f7d1de2d6a
Harvey Ramer <harvey@harveyramer.com>
2020-01-04 22:21:15 -0600

    Remove some sections to allow column alignment.

--- src/components/LatestPost.js
+++ src/components/LatestPost.js
@@ -15,32 +15,11 @@ class LatestPost extends React.Component {
         {posts &&
           posts.map(({ node: post }) => (
             <div key={post.id}>
-              <section className="section">
-
-                <div className="container content">
-                  <div className="columns">
-                    <div className="column is-12">
-                      <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-                        {post.frontmatter.title}
-                      </h1>
-                      <p>{post.frontmatter.description}</p>
-                      <PostContent content={post.html} />
-                      {post.frontmatter.tags && post.frontmatter.tags.length ? (
-                        <div style={{ marginTop: `4rem` }}>
-                          <h4>Tags</h4>
-                          <ul className="taglist">
-                            {post.frontmatter.tags.map(tag => (
-                              <li key={tag + `tag`}>
-                                <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                              </li>
-                            ))}
-                          </ul>
-                        </div>
-                      ) : null}
-                    </div>
-                  </div>
-                </div>
-              </section>
+              <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+                {post.frontmatter.title}
+              </h1>
+              <p>{post.frontmatter.description}</p>
+              <PostContent content={post.html} />
             </div>
           ))
         }
