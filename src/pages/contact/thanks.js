7082a9608c4d5bb1d6f4fe2f00ec9dc9f8f99232
Harvey Ramer <harvey@harveyramer.com>
2020-01-06 12:10:05 -0600

    layout improvements.

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -6,8 +6,10 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>I will get back to you as soon as possible.</p>
+          <div class="column is-10 is-offset-1">
+            <h1>Thank you!</h1>
+            <p>I will get back to you as soon as possible.</p>
+          </div>
         </div>
       </div>
     </section>
