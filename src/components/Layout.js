115e0bcdb59492ecb196bef4d087189017da80d9
Harvey Ramer <harvey@harveyramer.com>
2020-01-06 12:05:11 -0600

    Flexible footer. Thank you message for contact.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,41 +9,43 @@ import { withPrefix } from 'gatsby'
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
   return (
-    <div>
-      <Helmet>
-        <html lang="en" />
-        <title>{title}</title>
-        <meta name="description" content={description} />
+    <div class="site">
+      <div class="site__content">
+        <Helmet>
+          <html lang="en" />
+          <title>{title}</title>
+          <meta name="description" content={description} />
 
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-icon180x180.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
-          sizes="16x16"
-        />
-        <meta name="theme-color" content="#fff" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href={`${withPrefix('/')}img/apple-icon180x180.png`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`${withPrefix('/')}img/favicon-32x32.png`}
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href={`${withPrefix('/')}img/favicon-16x16.png`}
+            sizes="16x16"
+          />
+          <meta name="theme-color" content="#fff" />
 
-        <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
-        <meta
-          property="og:image"
-          content={`${withPrefix('/')}img/og-image.png`}
-        />
-      </Helmet>
-      <Navbar />
-      <div>{children}</div>
+          <meta property="og:type" content="business.business" />
+          <meta property="og:title" content={title} />
+          <meta property="og:url" content="/" />
+          <meta
+            property="og:image"
+            content={`${withPrefix('/')}img/og-image.png`}
+          />
+        </Helmet>
+        <Navbar />
+        <div>{children}</div>
+      </div>
       <Footer />
     </div>
   )
