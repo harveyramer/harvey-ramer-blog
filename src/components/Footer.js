e042edb00ef6eccdcd3e1f3bc63dd0922a2b1d89
Harvey Ramer <harvey@harveyramer.com>
2020-01-13 10:33:22 -0600

    Simplify the site. It's a blog.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
+// import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+// import vimeo from '../img/social/vimeo.svg'
 
 const Footer = class extends React.Component {
   render() {
