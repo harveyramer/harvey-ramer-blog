3430b884a9d452f4a8eae4e7f3e10d54bb3edf71
Harvey Ramer <harvey@harveyramer.com>
2020-01-24 10:53:25 -0600

    Mark up resume with more metadata.

--- src/templates/resume-page.js
+++ src/templates/resume-page.js
@@ -5,13 +5,36 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const ResumePageTemplate = ({ title, content, contentComponent }) => {
+export const ResumePageTemplate = ({ page, title, contentComponent, site }) => {
   const PageContent = contentComponent || Content
-
+  const author = page.frontmatter.author;
+  const nameTokens = author.split(" ");
   return (
     <div className="container">
-      <Helmet titleTemplate="%s">
+      <Helmet titleTemplate="%s | Resume">
         <title>{`${title}`}</title>
+        <meta
+          name="description"
+          content={`${page.frontmatter.description}`}
+        />
+        <meta
+          name="author"
+          content={`${author}`}
+        />
+        <meta
+          property="og:description"
+          content={`${page.frontmatter.description}`}
+        />
+        <meta
+          property="og:image"
+          content={`${site.siteMetadata.siteUrl}${page.frontmatter.image.childImageSharp.fluid.src}`}
+        />
+        <meta property="og:type" content="profile" />
+        <meta property="og:first_name" content={`${nameTokens[0]}`} />
+        <meta property="og:last_name" content={`${nameTokens[1]}`} />
+        <meta property="og:title" content={`${title}`} />
+        <meta property="og:url" content={`${site.siteMetadata.siteUrl}${page.fields.slug}`} />
+        <link rel="canonical" href={`${site.siteMetadata.siteUrl}${page.fields.slug}`} />
       </Helmet>
       <div className="columns">
         <div className="column is-10 is-offset-1">
@@ -19,7 +42,7 @@ export const ResumePageTemplate = ({ title, content, contentComponent }) => {
             <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
               {title}
             </h2>
-            <PageContent className="content" content={content} />
+            <PageContent className="content" content={page.html} />
           </div>
         </div>
       </div>
@@ -29,19 +52,21 @@ export const ResumePageTemplate = ({ title, content, contentComponent }) => {
 
 ResumePageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
-  content: PropTypes.string,
+  page: PropTypes.object,
   contentComponent: PropTypes.func,
+  site: PropTypes.object,
 }
 
 const ResumePage = ({ data }) => {
-  const { markdownRemark: post } = data
-
+  const { markdownRemark: post, site } = data
+  console.log(post)
   return (
     <Layout>
       <ResumePageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
-        content={post.html}
+        page={post}
+        site={site}
       />
     </Layout>
   )
@@ -56,9 +81,24 @@ export default ResumePage
 export const resumePageQuery = graphql`
   query ResumePage($id: String!) {
     markdownRemark(id: { eq: $id }) {
+      fields{ slug }
       html
       frontmatter {
         title
+        author
+        description
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+    site(siteMetadata: {siteUrl: {}}) {
+      siteMetadata{
+        siteUrl
       }
     }
   }
