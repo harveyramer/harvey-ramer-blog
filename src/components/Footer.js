5329a47c6aeeb00ae3ca7e21f48b63880b771939
Harvey Ramer <harvey@harveyramer.com>
2020-01-26 21:33:11 -0600

    Add linkedIn social icon to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 
 import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
-// import instagram from '../img/social/instagram.svg'
+import linkedin from '../img/social/linkedin.svg'
 import twitter from '../img/social/twitter.svg'
 // import vimeo from '../img/social/vimeo.svg'
 
@@ -61,6 +61,13 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
+                <a title="linkedin" href="https://linkedin.com/in/harveyramer">
+                  <img
+                    src={linkedin}
+                    alt="LinkedIn"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
                 <a title="facebook" href="https://facebook.com/harveyramer">
                   <img
                     src={facebook}
@@ -76,14 +83,7 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                {/* <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
+                {/* <a title="vimeo" href="https://vimeo.com">
                   <img
                     src={vimeo}
                     alt="Vimeo"
