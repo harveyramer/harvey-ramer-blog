abb1278e3b545e96fbc91badedb3dba206a27380
Harvey Ramer <harvey@harveyramer.com>
2020-01-28 20:58:27 -0600

    Add recommendations to resume.

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,7 @@ module.exports = {
       options: {
         develop: true, // Activates purging in npm run develop
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+        whitelist: ['blockquote']
       },
     }, // must be after other CSS plugins
     {
