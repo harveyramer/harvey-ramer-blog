8871d4702ef4eeaa97875afa29f7bd9a2bf5aa8c
Harvey Ramer <harvey@harveyramer.com>
2021-11-05 20:18:01 -0500

    Add feed image.
    Former-commit-id: 823ccaa91458c5001a34fd1fbc3d6e35c5cdb0b3

--- ogImg.js
+++ ogImg.js
@@ -4,27 +4,39 @@ const path = require('path');
 // path to the custom font (TTF or OTF only), relative to this file
 const PATH_TO_FILE = path.resolve(__dirname, 'dist/img/social_template.png');
 
+const api_key = process.env.CLOUDINARY_KEY;
+const api_secret = process.env.CLOUDINARY_SECRET;
+
+if (!api_key || !api_secret) {
+  console.error("Missing config.");
+};
+cloudinary.config({
+  cloud_name: "harveyramer",
+  api_key,
+  api_secret,
+});
 
 module.exports = {
-  make: ({ quote, slug, title }) => {
-    const api_key = process.env.CLOUDINARY_KEY;
-    const api_secret = process.env.CLOUDINARY_SECRET;
+  get: (filename) => {
     return new Promise((resolve, reject) => {
-      if (!api_key || !api_secret) {
-        reject("Missing config.");
+      if (!filename || !filename.length) {
+        reject("Missing properties.");
         return '';
-      };
+      }
+      cloudinary.search
+        .expression(`resource_type:image AND filename=${filename}`)
+        .max_results(1)
+        .execute().then(result => resolve(result.resources[0]?.secure_url));
+    });
+  },
+  make: ({ quote, slug, title }) => {
+    return new Promise((resolve, reject) => {
       if (!quote || !quote.length || !slug || !slug.length || !title || !title.length) {
         reject("Missing properties.");
         return '';
       }
       let text = encodeURIComponent(quote).replace(/%2C/g, '%E2%80%9A').replace(/%2F/g, '%E2%88%95');
       const titleText = encodeURIComponent(title).replace(/%2C/g, '%E2%80%9A').replace(/%2F/g, '%E2%88%95');
-      cloudinary.config({
-        cloud_name: "harveyramer",
-        api_key,
-        api_secret,
-      });
       cloudinary.uploader.upload(PATH_TO_FILE, {
         overwrite: true,
         alt: text,
