d0f6c35f8624a1fe5f1125fbf8586bd76917a45b
Harvey Ramer <harvey@harveyramer.com>
2024-01-23 20:43:26 -0500

    Figure out the function config.

--- function-config.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.copyFile('src/functions/_routes.json', 'functions/_routes.json', (err) => {
-  if (err) throw err;
-  console.log('src/functions/_routes.json was copied to functions.');
-});
